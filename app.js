064b4148230decaa4de372e7c523974ba634f2c6
jmaury <ajmh06@gmail.com>
2023-01-27 17:31:14 -0500

    first commit

--- app.js
+++ app.js
@@ -3,18 +3,25 @@ const app = express();
 
 const port = 3000;
 
+//motor de plantilla
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+
 app.use(express.static(__dirname + "/public"))
 
 app.get('/', (req, res) => {
-    res.send('mi respuesta desde express V.2')
+    res.render('index', {titulo: 'mi titulo dinámico'})
 })
 
 app.get('/servicios', (req, res) => {
-    res.send('estás en la página de servicios')
+    res.render('servicios', {titulo : 'estás en la página de servicios'})
 })
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(__dirname + "/public/404.html")
+    res.status(404).render('404', {
+        titulo: '404',
+        descripcion: 'titulo del sitio 404'
+    })
 })
 
 app.listen(port, () => {
